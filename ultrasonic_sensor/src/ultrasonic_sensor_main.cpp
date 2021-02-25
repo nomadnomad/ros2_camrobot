c6e50753b2040684ec5982fa2d85954df0ce965c
nomadnomd <nomadnomadnomad@icloud.com>
2021-02-25 23:42:24 +0900

    超音波センサー追加

---
+++ ultrasonic_sensor/src/ultrasonic_sensor_main.cpp
@@ -0,0 +1,31 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_lifecycle/lifecycle_node.hpp>
+#include <lifecycle_msgs/msg/state.hpp>
+#include "../include/ultrasonic_sensor/ultrasonic_sensor.hpp"
+
+int main(int argc, char * argv[]){
+  rclcpp::init(argc, argv);
+
+  /* 実行コンテキストであるExecutorの生成、この場合はSingleスレッドのExecutor */
+  rclcpp::executors::SingleThreadedExecutor exe;
+
+  std::shared_ptr<UltrasonicSensor> node = 
+    std::make_shared<UltrasonicSensor>();
+
+  RCLCPP_INFO(node->get_logger(), "インスタンス生成");
+
+  /* ノードのインスタンスを executorに登録 */
+  exe.add_node(node->get_node_base_interface());
+  RCLCPP_INFO(node->get_logger(), "ノードのインスタンスをexecutorに登録");
+  /******* ここまでがノードの初期化です　*****/
+
+  /* executorの実行ループを開始します。開始後は、Unconfigured状態です　*/
+  RCLCPP_INFO(node->get_logger(), "executorの実行ループを開始する");
+  exe.spin(); 
+  //rclcpp::spin(std::make_shared<TestImageSubscriber>());
+  RCLCPP_INFO(node->get_logger(), "shutdown()呼び出し前");
+  rclcpp::shutdown();
+  RCLCPP_INFO(node->get_logger(), "shutdown()呼び出し後");
+
+  return 0;
+}
